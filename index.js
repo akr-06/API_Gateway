edb768ed6a34e8136aa12c1f99470f3183caac12
Amit Kumar Roy <akumarroy86@gmail.com>
2023-01-17 09:15:11 +0530

    api gateway basic setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const axios = require('axios');
+const {PORT} = require('./serverConfig');
+
+const app = express();
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const e = require('express');
+
+const limiter = rateLimit({
+    windowMs : 2 * 60 * 1000,
+    max : 5,
+})
+
+app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice', async(req,res,next) => {
+    console.log(req.headers['x-access-token']);
+
+    try {
+            const response = await axios.get('http://localhost:3001/api/v1/isauthenticated',{
+            headers : {
+                'x-access-token' : req.headers['x-access-token']
+            }
+        })
+        console.log(response.data);
+        if(response.data.success){
+            next();
+        }
+        else{
+            return res.status(401).json({
+                message : "Unautorized"
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message : "Unauthorized"
+        })
+    }
+})
+
+app.use('/bookingservice',createProxyMiddleware({ target : 'http://localhost:3002/', changeOrigin: true}));
+
+app.get('/home',(req,res)=>{
+    res.json({message : "OK"});
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at port ${PORT}`);
+})
